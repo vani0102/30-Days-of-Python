61b58f1bf24e38bc309ac69688c8b82bce2f968c
Vani Sharma <vani1204sharma@gmail.com>
2026-09-17 15:21:37 +0530

    Add variables and print their types in Python

---
+++ Day-01-Variables/variables.py
@@ -0,0 +1,16 @@
+# Day 1 - Variables and Data Types
+
+name = "Vani"
+age = 22
+cgpa = 8.0
+is_student = True
+
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
